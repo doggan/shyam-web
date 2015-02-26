6d76e5851b0f7a78fdde8590879c3f97a3f1643d
Shyam Guthikonda <shyamguth@gmail.com>
2015-02-26 23:21:11 +0900

    Implement smooth scrolling.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -17,3 +17,16 @@ $(document).ready(function() {
     $(this).text(q);
   });
 });
+
+$('.smoothscroll').on('click', function(e) {
+  e.preventDefault();
+
+  var target = this.hash,
+    $target = $(target);
+
+  $('html, body').stop().animate({
+    'scrollTop': $target.offset().top
+  }, 800, 'swing', function() {
+    window.location.hash = target;
+  });
+});
