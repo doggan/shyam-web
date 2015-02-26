920388012db8b19c13e16aae954e05034a56b836
Shyam Guthikonda <shyamguth@gmail.com>
2015-02-26 13:40:32 +0900

    Add email address obfuscation.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,19 @@
-console.log('\'Allo \'Allo!');
+'use strict';
+
+$(document).ready(function() {
+  // Email address obfuscation:
+  var e = 'shyamguth';
+  var t = 'gmail';
+  var n = '.com';
+  var q = e + '@' + t + n;
+  var r = 'mailto:' + q;
+
+  // Set mailto links:
+  $('.contact-email').each(function() {
+    $(this).attr('href', r);
+  });
+  // Set email display text:
+  $('.contact-email-text').each(function() {
+    $(this).text(q);
+  });
+});
