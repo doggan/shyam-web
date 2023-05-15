b78c2723ba513b49e317d5dd3265534e157e88b3
Shyam Guthikonda <shyamguth@gmail.com>
2023-05-14 21:25:25 -0700

    Fix remaining linter errors for build

--- src/components/tech-tag.tsx
+++ src/components/tech-tag.tsx
@@ -1,4 +1,3 @@
-import { UnreachableCaseError } from '@/utils/unreachable-error';
 import { clsx } from 'clsx';
 
 export enum TechName {
@@ -39,73 +38,10 @@ interface ITechTagProps {
   name: TechName;
 }
 
-function getTechTagColors(name: TechName): string {
-  switch (name) {
-    case TechName.Angular:
-      return 'text-blue-600 bg-blue-200';
-    case TechName.HTML:
-      return 'text-blue-600 bg-blue-200';
-    case TechName.CSS:
-      return 'text-blue-600 bg-blue-200';
-    case TechName.AWS:
-      return 'text-blue-600 bg-blue-200';
-    case TechName.C:
-      return 'text-blue-600 bg-blue-200';
-    case TechName.CoffeeScript:
-      return 'text-blue-600 bg-blue-200';
-    case TechName.CPlusPlus:
-      return 'text-blue-600 bg-blue-200';
-    case TechName.CSharp:
-      return 'text-blue-600 bg-blue-200';
-    case TechName.GCP:
-      return 'text-blue-600 bg-blue-200';
-    case TechName.GLSL:
-      return 'text-blue-600 bg-blue-200';
-    case TechName.Go:
-      return 'text-blue-600 bg-blue-200';
-    case TechName.GraphQL:
-      return 'text-blue-600 bg-blue-200';
-    case TechName.Java:
-      return 'text-blue-600 bg-blue-200';
-    case TechName.JavaScript:
-      return 'text-blue-600 bg-blue-200';
-    case TechName.Lua:
-      return 'text-blue-600 bg-blue-200';
-    case TechName.MySQL:
-      return 'text-blue-600 bg-blue-200';
-    case TechName.Node:
-      return 'text-blue-600 bg-blue-200';
-    case TechName.ObjectiveC:
-      return 'text-blue-600 bg-blue-200';
-    case TechName.OpenGL:
-      return 'text-blue-600 bg-blue-200';
-    case TechName.PHP:
-      return 'text-blue-600 bg-blue-200';
-    case TechName.Python:
-      return 'text-blue-600 bg-blue-200';
-    case TechName.React:
-      return 'text-blue-600 bg-blue-200';
-    case TechName.Terraform:
-      return 'text-blue-600 bg-blue-200';
-    case TechName.ThreeJS:
-      return 'text-blue-600 bg-blue-200';
-    case TechName.TorqueNetworkLibrary:
-      return 'text-blue-600 bg-blue-200';
-    case TechName.TypeScript:
-      return 'text-blue-600 bg-blue-200';
-    case TechName.Unity:
-      return 'text-blue-600 bg-blue-200';
-    case TechName.UnrealEngine3:
-      return 'text-blue-600 bg-blue-200';
-    case TechName.UnrealScript:
-      return 'text-blue-600 bg-blue-200';
-    case TechName.Vue:
-      return 'text-blue-600 bg-blue-200';
-    case TechName.XNA:
-      return 'text-blue-600 bg-blue-200';
-    default:
-      throw new UnreachableCaseError(name);
-  }
+function getTechTagColors(_: TechName): string {
+  // I originally thought of returning a different color depending on the tech; for now
+  // just keep them all the same.
+  return 'text-blue-600 bg-blue-200';
 }
 
 export default function TechTag({ name }: ITechTagProps) {
--- src/utils/unreachable-error.ts
+++
@@ -1,5 +0,0 @@
-export class UnreachableCaseError extends Error {
-    constructor(val: never | undefined) {
-        super(`Unreachable case: ${JSON.stringify(val)}`);
-    }
-}
